53ce963430556387bd384d0781995f4c32827c08
Logan White <git@loganwhite.org>
2026-02-04 01:17:39 +0000

    Sort patches descending

--- public/rom-patcher-js/RomPatcher.webapp.js
+++ public/rom-patcher-js/RomPatcher.webapp.js
@@ -6,19 +6,19 @@
 * License:
 *
 * MIT License
-* 
+*
 * Copyright (c) 2016-2025 Marc Robledo
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 * copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all
 * copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -1408,7 +1408,7 @@ const ZIPManager = (function (romPatcherWeb) {
 				/* success */
 				function (zipReader) {
 					zipReader.getEntries(function (zipEntries) {
-						const filteredEntries = _filterEntriesPatches(zipEntries);
+						const filteredEntries = _filterEntriesPatches(zipEntries).sort((a, b) => b.filename.localeCompare(a.filename));
 
 						if (filteredEntries.length) {
 							const selectablePatches = [];
