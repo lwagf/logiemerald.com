1a8411ad786b794b20c2a9a103c765d6a1d57c78
Logan White <git@loganwhite.org>
2026-02-04 02:04:24 +0000

    Need to use hash routing for gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter, createHashHistory } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import './styles/globals.css'
 
-const router = createRouter({ routeTree })
+const hashHistory = createHashHistory()
+
+const router = createRouter({ 
+  routeTree,
+  history: hashHistory,
+})
 
 declare module '@tanstack/react-router' {
   interface Register {
